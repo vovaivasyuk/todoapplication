a3e010b08562263659f1cc68b86c477c34fa4b81
vovaivasyuk <vovaivasyuk@gmail.com>
2019-08-08 00:16:11 +0300

    ToDoApp070819_0016

--- platforms/browser/www/js/services/todo.js
+++ platforms/browser/www/js/services/todo.js
@@ -1,9 +1,7 @@
 angular.module('todoApp.services',[])
 	.factory('Todos', function() {
 		var items = [
-			{text:'first', done:true, details:'',date:''},
-			{text:'Second', done:false,details:'',date:''},
-			{text:'Third', done:false, details:'',date:''}
+			
 			]
 			return items;
 	})
